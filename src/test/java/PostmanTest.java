aa0321fc6ebf9e1588ee0bcf7c855f6aba2ce2be
Cryofbb <78804095+Cryofbb@users.noreply.github.com>
2021-07-20 17:24:55 +0300

    Postman test work #1

--- src/test/java/PostmanTest.java
+++ src/test/java/PostmanTest.java
@@ -14,7 +14,7 @@ public class PostmanTest {
                 .post("/post")
                 .then()
                 .statusCode(200)
-                .body("data", equalTo("melon"))
+                .body("data", equalTo("watermelon"))
         ;
     }
 }
